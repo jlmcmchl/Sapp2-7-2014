97ac904368b74c91684c7a0fac85c5855983520c
Team RUSH 1 <TeamRUSH1@Team-RUSH-1s-MacBook-Pro.local>
2014-02-07 09:04:55 -0500

    Initial Commit

---
+++ Sapp2-7-2014/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sapp2-7-2014
+//
+//  Created by Team RUSH 1 on 2/7/14.
+//  Copyright (c) 2014 Team RUSH 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sapp2-7-2014Tests/Sapp2_7_2014Tests.m
@@ -0,0 +1,34 @@
+//
+//  Sapp2_7_2014Tests.m
+//  Sapp2-7-2014Tests
+//
+//  Created by Team RUSH 1 on 2/7/14.
+//  Copyright (c) 2014 Team RUSH 1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sapp2_7_2014Tests : XCTestCase
+
+@end
+
+@implementation Sapp2_7_2014Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
